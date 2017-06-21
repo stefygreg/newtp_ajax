e1aaadc78e649c01dfe6609e47cc85c8acb24b0f
littleAsteria <cinnamonpan@gmail.com>
2017-06-21 17:07:02 +0200

    ajout ajax repetoire

---
+++ basic-87/bootstrap/tutojs.js
@@ -0,0 +1,15 @@
+document.querySelector('#cp')
+var cp = document.querySelector('#cp')
+
+
+console.log(cp.value)
+
+
+/*document.querySelector('#form').addEventListener('submit', funciton(e){
+	var cp = document.querySelector('#cp')
+	if (cp.value.lenght !=5) {
+			alert('Le code postal n\'est pas bon')
+	}
+
+
+})*/
